8f241eae06387924876dc7f15de051d174167576
Andrii Kapran <kapranandriyu@gmail.com>
2023-07-04 22:01:54 +0200

    changed projects descriptions

--- back/data/projects.js
+++ back/data/projects.js
@@ -13,7 +13,7 @@ const certificatesListData = [
     key: "med-purchases",
     path: 'projects/med-purchases',
     title: "Med Purchases",
-    description: "Work with Angular and PrimeNG like UI library",
+    description: `Med Purchases is a web application developed using Angular and the PrimeNG UI library. It provides a user-friendly platform for managing medical purchases. The project includes features like advanced search, filtering, and sorting to simplify the management of medical procurement. It is built with TypeScript, HTML5, SASS/SCSS, and RxJS to ensure a seamless user experience and efficient development.`,
     imagesPaths: ['/files/projects/med-purchases/home-page.png', '/files/projects/med-purchases/search.png', '/files/projects/med-purchases/search-and-filter.png', '/files/projects/med-purchases/filter-and-sort.png', '/files/projects/med-purchases/hide-columns.png'],
     linkToCode: 'https://github.com/AKapran8/Medical-Purchases',
     nodeVersion: '18.16.0',
@@ -22,10 +22,10 @@ const certificatesListData = [
   {
     key: "calculator",
     path: 'projects/calculator',
-    title: "React Calculator",
-    description: "In current project I use these technologies: ReactJS, TypeScript, TSX, JavaScript, JSX. For styling I use styled-components package",
+    title: "Calculator",
+    description: "This project using React with the styled-components package for UI styling. It provides a user-friendly interface for performing basic arithmetic calculations.",
     imagesPaths: ['/files/projects/calculator/empty.png', '/files/projects/calculator/plus.png', '/files/projects/calculator/minus.png'],
-    linkToCode: 'https://github.com/AKapran8/calculator',
+    linkToCode: 'https://github.com/AKapran8/Calculator',
     nodeVersion: '18.14.0',
     technologies: [TG.ReactJS, TG.TypeScript, TG.TSX, TG.JSX, TG.JavaScript, TG.HTML5, TG["SASS/SCSS"]]
   },
